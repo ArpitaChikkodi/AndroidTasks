cfa84b2e59325f4aca74ce32e25e0cddfd732b8e
Arpita Chikkodi <apschikkodi14@gmail.com>
2020-03-21 17:44:49 +0530

    init

--- NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MainActivity.java
@@ -7,7 +7,14 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+/*
+    This application fetches news from newsapi-google API Key
+    It fetches the title and description and shows it in fragment at runtime
+    It uses internet permission!
+    Fragment life cycle is demonstrated!
+ */
 public class MainActivity extends AppCompatActivity {
 
     Button btnEnglish;
@@ -23,12 +30,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 frag1 = new FragmentEnglish();
+                Toast.makeText(MainActivity.this,"Fragment called!",'1').show();
                 FragmentManager fm = getSupportFragmentManager();
+                Toast.makeText(MainActivity.this,"Fragment oncreate()!",'1').show();
 // create a FragmentTransaction to begin the transaction and replace the Fragment
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
                 fragmentTransaction.add(R.id.llayout, frag1);
                 fragmentTransaction.commit(); // save the changes
+                Toast.makeText(MainActivity.this,"Fragment onResume()!",'2').show();
             }
         });
 
